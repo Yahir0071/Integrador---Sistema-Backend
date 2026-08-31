d4db9ae666051bbdd769fe7ff986661cba40f23d
Yahir <yhuairayauri@gmail.com>
2026-08-30 23:06:49 -0500

    Hola

---
+++ src/main/java/pe/edu/utp/Grupo06/Grupo06Application.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.Grupo06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo06Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/utp/Grupo06/Grupo06ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.Grupo06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
